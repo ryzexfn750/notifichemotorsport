776e871fd34a7ff8e3e65ca3201c2424603618c2
Ryze <ninettino150@gmail.com>
2026-07-05 13:14:59 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-import os 
+import os
 
-BOT_TOKEN = os.getenv("8931387537:AAGhfjNQwpy-CWE96g4La6NPlTpLXQZXCvE")
-CHAT_ID = os.getenv("-5063780890")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
 
 F1_ICS_URL = "https://ics.ecal.com/ecal-sub/6a4a2b5a3a2f8e0002297ae4/Formula%201.ics"
 MOTOGP_ICS_URL = "motogp-calendar_p1_p2_practice_qualifying1_qualifying2_sprint_warmup_race.ics"
